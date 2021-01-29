1881b66e83b38b4f9ccd18c03458cbe3e295b8c9
Sean Dougherty <sean@lupinehook.com>
2021-01-29 08:29:47 -0800

    new category template to display categories. updated main index to list most viewed/liked cats/pages. updated views to pull that info and urls to account for /category/slug/ routing

--- portal/urls.py
+++ portal/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import portal_about, portal_index
+from .views import portal_about, portal_index, portal_show_category
 
 
 app_name = "portal"
@@ -7,4 +7,5 @@ app_name = "portal"
 urlpatterns = [
     path("", portal_index, name="portal_index"),
     path("about/", portal_about, name="portal_about"),
+    path("category/<slug:slug>/", portal_show_category, name="portal_show_category"),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Page
 
 
 def portal_index(request):
     category_list = Category.objects.order_by("-likes")
-    context = {"boldmessage": "why hello there", "category_list": category_list}
+    page_list = Page.objects.order_by("-views")[:5]
+    context = {"boldmessage": "why hello there", "category_list": category_list, "page_list": page_list}
     return render(request, "portal/index.html", context=context)
 
 
 def portal_about(request):
     return render(request, "portal/about.html", {"aboutmessage": "welcome to about page."})
+
+
+def portal_show_category(request, slug):
+    context = {}
+
+    try:
+        category = Category.objects.get(slug=slug)
+        pages = Page.objects.filter(category=category)
+        context["category"] = category
+        context["pages"] = pages
+
+    except Category.DoesNotExist:
+        context["category"] = None
+        context["pages"] = None
+
+    return render(request, "portal/category.html", context=context)
